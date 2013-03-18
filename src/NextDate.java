85080a149db86e5802bc6565bbf90b8ea7bf648c
kevin <kevin@kevin-dell>
2013-03-18 14:14:24 -0700

    change the date to Mar 1, 2013

--- src/NextDate.java
+++ src/NextDate.java
@@ -1,7 +1,7 @@
 public class NextDate {
 
         public static void main(String[] args) {
-        	    int d=10, m=3, y=2013;
+        	    int d=1, m=3, y=2013;
                 Date date = new Date(m, d, y);
                 date.increment();
                 System.out.println(date);
